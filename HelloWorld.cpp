931c8e79bb99af43daf8b3626b8ad8ce4eec687a
Ryan Dilley <rdilley@unomaha.edu>
2025-10-01 15:42:16 -0500

    First commit for conflict

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,7 @@
 // Simple C++ program to display "Hello World" 
 //Author: Ryan Dilley <rdilley@unomaha.edu>
 // Header file for input output functions 
+#github and git conflict
 #include<iostream>  
   
 using namespace std; 
